a2740393c05c74b887f42d911b4c836256197154
Travis Geiselbrecht <geist@foobox.com>
2010-02-04 10:16:14 +0000

    Move the ray file structures to a header file
    git-svn-id: svn+ssh://svn.tkgeisel.com/var/svn/mysrc@840 0e4dd39d-42fa-0310-8bbd-9655faafb336

---
+++ RayFile.h
@@ -0,0 +1,24 @@
+#ifndef __RAYFILE_H
+#define __RAYFILE_H
+
+#define RAY_HEADER_MAGIC 'RAY1'
+
+struct RayHeader {
+	uint32_t magic;
+	uint32_t width;
+	uint32_t height;
+};
+
+enum RayDataType {
+	TYPE_NULL = 0,
+	TYPE_PIXEL,
+	TYPE_EOF,
+};
+
+struct RayDataPixel {
+	uint32_t x;
+	uint32_t y;
+	float r, g, b;
+};
+
+#endif
